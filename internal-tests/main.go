30249293823cd18ff79a65117327b2221ceefa18
KOKUMUbooker <bookerochieng@gmail.com>
2025-11-22 10:24:25 +0300

    Add internal test sections

---
+++ internal-tests/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"piscine"
+)
+
+func main() {
+	// ========= QuadA test ========
+	fmt.Println("====== Quad A ======")
+	piscine.QuadA(5, 3)
+	fmt.Println("")
+	piscine.QuadA(5, 1)
+	fmt.Println("")
+	piscine.QuadA(5, 1)
+	fmt.Println("")
+	piscine.QuadA(1, 1)
+
+	// ========= QuadB test ========
+	fmt.Println("====== Quad B ======")
+	piscine.QuadB(5, 3)
+	fmt.Println("")
+	piscine.QuadB(5, 1)
+	fmt.Println("")
+	piscine.QuadB(5, 1)
+	fmt.Println("")
+	piscine.QuadB(1, 1)
+
+	// ========= QuadC test ========
+	fmt.Println("====== Quad C ======")
+
+	// ========= QuadD test ========
+	fmt.Println("====== Quad D ======")
+
+	// ========= QuadE test ========
+	fmt.Println("====== Quad E ======")
+}
