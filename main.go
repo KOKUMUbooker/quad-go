d4c50397998c36d9edd4f5c27dfeea2ffcbb6f69
Ryan Ochieng <ryochieng@noreply.learn.zone01kisumu.ke>
2025-11-22 18:31:30 +0000

    Add quad4

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
-package main
+package piscine
 
-func QuadA(x,y int) {
-	if x<= 0 || y <= 0 {
+import "github.com/01-edu/z01"
+
+func QuadD(x, y int) {
+	// input validation
+	if x <= 0 || y <= 0 {
 		return
-	} else {
-		for row := 1; row <= y; row++ {
-			if row == 1{
-				for col := 1; col <= x; col++ {
-					//choose character
-				}
-				//print new line
-			} else if row == y {
-				//print bottom row
-			} else {
-				//print middle row
-			}
-		}
 	}
 
-}
+	// i is current row number
+
+	for i :=1 ; i <= y ; i++ {
+		// j is current col number
+		for j := 1 ; j <= x ; j++ {
+			// holds char to print
+			var charToPrint rune = ''
+
+			// identify current position is a border
+			isTopRow := i == 1
+			isBottomRow := i == y
+			isLeftCol := j == 1
+			isRightCol := j == x
+
+			// Character Logic
+			if isTopRow && isLeftCol {
+				charToPrint = 'A'
+			} else if isTopRow && isRightCol {
+				charToPrint = 'C'
+			} else if isBottomRow && isLeftCol {
+				charToPrint = 'A'
+			} else if isBottomRow && isRightCol{
+				charToPrint = 'C'
+			} else if isTopRow || isBottomRow || isLeftCol || isRightCol {
+				charToPrint = 'B'
+			} else {
+				charToPrint = ' '
+			}
+
+			z01.PrintRune(charToPrint)
+		}
+
+		z01.PrintRune('\n')
+	}
+	
