6dc4ee029df38f1b238862cb006c74f6e5abe406
hetyang <hakongo886@gmail.com>
2025-11-23 15:28:19 +0300

    Refactor code for A,B,C

--- quadC.go
+++ quadC.go
@@ -1,34 +1,20 @@
 package piscine
 
 import "fmt"
-// piscine.QuadC(5,3)
-//ABBBA
-//B   B
-//CBBBC
 
-// column = 5
-// rows = 3
-
-func QuadC(x,y int) {
-	for i := 1;i <= 3;i ++ {
-		for j :=1 ;j <= x; j ++{
-			if i==1 &&  j==1{
+// piscine.QuadC(3,2)
+func QuadC(x, y int) {
+	for i := 1; i <= y; i++ {
+		for j := 1; j <= x; j++ {
+			if i == 1 && (j == 1 || j == x) { // Top right & left corners
 				fmt.Printf("A")
-			} else if i == 1 && j== x {
-				fmt.Printf("A")
-			} else if i == y && j == 1 {
+			} else if i == y && (j == 1 || j == x) { // bottom left & right corners
 				fmt.Printf("C")
-			} else if  i==y && j == x {
-				fmt.Printf("C")
-			}else if j==1 && i>1 && i < y {
-				fmt.Printf("B") 
-			}else if j==x && i > 1 && i< y{
+			} else if (i > 1 && i < y) && (j == 1 || j == x) { // left & right non corner borders
 				fmt.Printf("B")
-			}else if i==1 && j>1 && j< x{
+			} else if (j > 1 && j < x) && (i == 1 || i == y) { // top & bottom non corner borders
 				fmt.Printf("B")
-			}else if i==y && j >1 && j< x{
-				fmt.Printf("B")
-			}else {
+			} else {
 				fmt.Print(" ")
 			}
 		}
