a13bcc353642468c1597eebc2074d3125488fcd0
KOKUMUbooker <bookerochieng@gmail.com>
2025-11-24 06:22:19 +0300

    Add all tests

--- test/main.go
+++ test/main.go
@@ -1,8 +1,54 @@
 package main
 
-import "piscine"
+import (
+	"fmt"
+
+	"piscine"
+)
 
 func main() {
-	// piscine.QuadA(5,1)
-	piscine.QuadD(5,3)
+	fmt.Println("====== Quad A ======")
+	piscine.QuadA(5, 3)
+	fmt.Println("")
+	piscine.QuadA(5, 1)
+	fmt.Println("")
+	piscine.QuadA(1, 1)
+	fmt.Println("")
+	piscine.QuadA(1, 5)
+
+	fmt.Println("\n====== Quad B ======")
+	piscine.QuadB(5, 3)
+	fmt.Println("")
+	piscine.QuadB(5, 1)
+	fmt.Println("")
+	piscine.QuadB(1, 1)
+	fmt.Println("")
+	piscine.QuadB(1, 5)
+
+	fmt.Println("\n====== Quad C ======")
+	piscine.QuadC(5, 3)
+	fmt.Println("")
+	piscine.QuadC(5, 1)
+	fmt.Println("")
+	piscine.QuadC(1, 1)
+	fmt.Println("")
+	piscine.QuadC(1, 5)
+
+	fmt.Println("\n====== Quad D ======")
+	piscine.QuadD(5, 3)
+	fmt.Println("")
+	piscine.QuadD(5, 1)
+	fmt.Println("")
+	piscine.QuadD(1, 1)
+	fmt.Println("")
+	piscine.QuadD(1, 5)
+
+	fmt.Println("\n====== Quad E ======")
+	piscine.QuadE(5, 3)
+	fmt.Println("")
+	piscine.QuadE(5, 1)
+	fmt.Println("")
+	piscine.QuadE(1, 1)
+	fmt.Println("")
+	piscine.QuadE(1, 5)
 }
